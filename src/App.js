6f473e75fde94285db641887b64d7d102eb9d9e0
tymatson109 <tymatson109@gmail.com>
2021-03-30 08:05:41 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import Footer from './components/Footer';
+import Header from './components/Header';
+import Game from './components/Game';
+import './components/components.css';
+import Button from '@material-ui/core/Button'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+
+    const [users, setUsers] = useState(null)
+    const [reset, setReset] = useState(false)
+
+    const resetClicked = () => {
+        setReset(!reset)
+    }
+
+    return (
+        <div className="app">
+            <div className="app__header">
+                <Header currentUser={setUsers}/>
+            </div>
+            <div className="app__game">
+                {users
+                ? (
+                    <div>
+                        <Game reset={reset} />
+                        <div className="app__reset">
+                            <Button onClick={resetClicked} >Reset</Button>
+                        </div>
+                    </div>
+                )
+                : null
+                }
+            </div>
+            <div className="app__footer">
+                <Footer users={users} />
+            </div>
+        </div>
+    )
 }
 
-export default App;
+export default App
